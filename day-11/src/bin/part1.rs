eda58d212a3cd510e5d939ce84bb2877e5f02790
David <david.abraham.1337@pm.me>
2023-12-14 20:54:48 -0500

    Add part 1 of day 11

--- day-11/src/bin/part1.rs
+++ day-11/src/bin/part1.rs
@@ -5,33 +5,66 @@ fn main() {
 }
 
 fn parse_input(input: &str) -> Vec<(i32, i32)> {
-    let (mut max_x, mut max_y) = (0, 0);
+    let lines: Vec<&str> = input.lines().collect();
+    let (max_x, max_y) = (
+        lines.len() - 1,
+        lines[0].chars().collect::<Vec<char>>().len() - 1,
+    );
     let gals: Vec<(i32, i32)> = input
         .lines()
         .enumerate()
         .flat_map(|(y, line)| {
-            max_y += 1;
             line.chars()
                 .enumerate()
-                .inspect(|_c| {
-                    max_x += 1;
-                })
-                .filter(|(x, c)| *c == '#')
-                .map(|(x, c)| (x as i32, y as i32))
+                .filter(|(_x, c)| *c == '#')
+                .map(|(x, _c)| (x as i32, y as i32))
                 .collect::<Vec<(i32, i32)>>()
         })
         .collect::<Vec<(i32, i32)>>();
 
-    max_y -= 1;
-    dbg!(max_y, max_x / max_y);
-    //let empty_rows = (0..gals.iter().map(|(x, y)| y)
-    gals
+    let empty_rows: Vec<i32> = (0..=max_y as i32)
+        .filter(|y| {
+            !gals
+                .iter()
+                .map(|(_x, y)| *y)
+                .collect::<Vec<i32>>()
+                .contains(&(*y as i32))
+        })
+        .collect();
+
+    let empty_cols: Vec<i32> = (0..=max_x as i32)
+        .filter(|x| {
+            !gals
+                .iter()
+                .map(|(x, _y)| *x)
+                .collect::<Vec<i32>>()
+                .contains(&(*x as i32))
+        })
+        .collect();
+
+    let expanded_gals: Vec<(i32, i32)> = gals
+        .iter()
+        .map(|(x, y)| {
+            let nb_empty_cols_smaller = empty_cols.iter().filter(|ec| *ec < x).count() as i32;
+            let nb_empty_rows_smaller = empty_rows.iter().filter(|er| *er < y).count() as i32;
+            (x + nb_empty_cols_smaller, y + nb_empty_rows_smaller)
+        })
+        .collect();
+    expanded_gals
 }
 
 fn part1(input: &str) -> String {
     let gals = parse_input(input);
-    dbg!(gals);
-    "".to_string()
+    let mut sum = 0;
+    for i in 0..gals.len() {
+        for j in i..gals.len() {
+            if i == j {
+                continue;
+            }
+            sum += (gals[i].0 - gals[j].0).abs() + (gals[i].1 - gals[j].1).abs();
+        }
+    }
+    sum.to_string()
 }
 
 #[cfg(test)]
@@ -42,15 +75,15 @@ mod tests {
     fn test() {
         let result = part1(
             "...#......
-        .......#..
-        #.........
-        ..........
-        ......#...
-        .#........
-        .........#
-        ..........
-        .......#..
-        #...#.....",
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....",
         );
         assert_eq!(result, "374".to_string());
     }
